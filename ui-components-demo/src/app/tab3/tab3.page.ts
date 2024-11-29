c4f00e2c55b345ec49d9494c454a63595d658934
Eoin Concannon <101334026+EoinConcannon@users.noreply.github.com>
2024-11-29 10:53:30 +0000

    working on part 5

--- ui-components-demo/src/app/tab3/tab3.page.ts
+++ ui-components-demo/src/app/tab3/tab3.page.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonModal, IonButtons, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { createAnimation } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonModal,
+    IonButtons,
+    IonItem,
+    IonLabel,
+    IonInput,
+    ExploreContainerComponent
+  ],
 })
 export class Tab3Page {
   constructor() {}
+
+  enterAnimation(baseEl: any) {
+    const backdropAnimation = createAnimation()
+      .addElement(baseEl.querySelector('ion-backdrop')!)
+      .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+
+    const wrapperAnimation = createAnimation()
+      .addElement(baseEl.querySelector('.modal-wrapper')!)
+      .keyframes([
+        { offset: 0, opacity: '0', transform: 'translateY(100%)' },
+        { offset: 1, opacity: '0.99', transform: 'translateY(0)' }
+      ]);
+
+    return createAnimation()
+      .addElement(baseEl)
+      .easing('ease-out')
+      .duration(500)
+      .addAnimation([backdropAnimation, wrapperAnimation]);
+  }
+
+  leaveAnimation(baseEl: any) {
+    return this.enterAnimation(baseEl).direction('reverse');
+  }
 }
