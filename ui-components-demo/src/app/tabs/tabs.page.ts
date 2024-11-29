e4e8a51dbf83317fed18a7e7936b4466bd3ea0fa
Eoin Concannon <101334026+EoinConcannon@users.noreply.github.com>
2024-11-29 10:03:48 +0000

    added settings tab and icon

--- ui-components-demo/src/app/tabs/tabs.page.ts
+++ ui-components-demo/src/app/tabs/tabs.page.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, person, notifications } from 'ionicons/icons';
+import { home, person, notifications, settingsOutline } from 'ionicons/icons'; // Import settingsOutline icon
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge], // Added IonBadge to imports
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge],
 })
 export class TabsPage {
   notificationCount = 3; // This will be displayed in the badge
   
   constructor() {
-    addIcons({ home, person, notifications });
+    addIcons({ home, person, notifications, settingsOutline }); // Add settingsOutline icon
   }
 }
--- ui-components-demo/src/app/tabs/tabs.routes.ts
+++ ui-components-demo/src/app/tabs/tabs.routes.ts
@@ -22,6 +22,11 @@ export const routes: Routes = [
           import('../tab3/tab3.page').then((m) => m.Tab3Page),
       },
       {
+        path: 'settings',
+        loadComponent: () =>
+          import('../settings/settings.page').then((m) => m.SettingsPage), // Add this line
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full',
