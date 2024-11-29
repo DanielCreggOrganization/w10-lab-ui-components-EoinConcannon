8376a90156d2bd7b81d10430b5ea002413767ce0
Eoin Concannon <101334026+EoinConcannon@users.noreply.github.com>
2024-11-29 10:58:28 +0000

    added alert and toast messages to settings page

--- ui-components-demo/src/app/settings/settings.page.ts
+++ ui-components-demo/src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { AlertController, ToastController } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,8 +8,37 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    ExploreContainerComponent
+  ],
 })
 export class SettingsPage {
-  constructor() {}
+  constructor(
+    private alertController: AlertController,
+    private toastController: ToastController
+  ) {}
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'This is a toast message',
+      duration: 2000,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
 }
